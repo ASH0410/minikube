da2f2781e3f085ddc80a556a6274655a316b09a1
Ashish Tripathi <ashish.tripathi@ihsmarkit.com>
2022-10-14 14:05:41 +0530

    updated app.py

--- app1/flask-docker-app1/app.py
+++ app1/flask-docker-app1/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     html = "<h2>Hello {name},</h2>" \
-           "<h3>Welcome to Flask-App-ONE !!</h3>" \ 
+           "<h3>Welcome to Flask-App-ONE !!</h3>" \
            "<b>Pod-Hostname:</b> {hostname}<br/>"
     return html.format(name=os.getenv('NAME'), hostname=socket.gethostname())
 
